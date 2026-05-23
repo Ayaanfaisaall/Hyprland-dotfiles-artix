a52460d11b9b7589f7a7c0e0defce63aeb4a7253
Ayaanfaisaall <ayaanfaisaall@gmail.com>
2026-05-23 22:56:25 +0500

    v2.0, d3.0

---
+++ .config/hypr/scripts/controlh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+a=1
+
+if [[ $a -eq 1 ]]; then
+    echo ""
+    sed -i "3s/a=1/a=0/" ~/dotfiles/.config/waybar/scripts/controls.sh
+else
+    echo "[control]"
+    sed -i "3s/a=0/a=1/" ~/dotfiles/.config/waybar/scripts/controls.sh
+fi
